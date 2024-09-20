7d7424a6e1b6f6f39a79ccde33188a5697fecd66
sontv <sontv@MacBook-Pro-cua-sontv.local>
2024-09-20 13:25:16 +0700

    B5

--- B5.js
+++ B5.js
@@ -4,8 +4,8 @@
     export default
     2. Destructuring Array
     3. Destructuring Object
-    4. Spread Operator
-    5. Rest Params
+    4. Spread Operator ...
+    5. Rest Params ...
 */
 // import { myName, addProduct, products } from "./data.js";
 // import getData from "./data.js";
@@ -15,10 +15,10 @@
 // addProduct();
 // getData();
 
-const myNumber = [1, 2, 3];
+// const myNumber = [1, 2, 3];
 
-const [a, c, b] = myNumber;
-console.log(a, b, c);
+// const [a, c, b] = myNumber;
+// console.log(a, b, c);
 
 // const a = myNumber[0];
 // const b = myNumber[1];
@@ -32,7 +32,53 @@ console.log(a, b, c);
 // const [iphone, xiaomi, samsung] = products;
 // console.log(iphone);
 
-const iphone = { id: 1, productName: "iphone", price: 100 };
+// const iphone = { id: 1, productName: "iphone", price: 100 };
 
-const { id, productName } = iphone;
-console.log(id, price, productName);
+// const { id, productName } = iphone;
+// console.log(id, price, productName);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const vegetable = ["Carrot", "Tomato", "Cucumber"];
+
+// // const newArr = fruits.concat(vegetable);
+// const newArr = [...fruits, ...vegetable];
+// console.log(newArr);
+
+// const newFruits = [...fruits, "abcd", "hhhh"];
+// // const newFruits = fruits;
+// console.log(newFruits);
+
+// const sum = (...rest) => {
+//   console.log(rest);
+// };
+// sum(1, 2, 3);
+
+// const info = {
+//   id: 1,
+//   title: "Tieu de 1",
+//   author: "sontv",
+//   desc: "description",
+// };
+
+// const { id, desc, ...other } = info;
+// console.log("id:", id);
+// console.log("other:", other);
+
+const data = [
+  {
+    id: 1,
+    title: "Tieu de 1",
+    author: "sontv",
+    desc: "description",
+  },
+  {
+    id: 2,
+    title: "Tieu de 2",
+    author: "datlt",
+    desc: "description 2",
+  },
+];
+
+const [sontv, datlt] = data;
+const { id, title } = sontv;
+console.log(id, title);
