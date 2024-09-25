0ad5b5a66840c12fdcaf730da8fba89d89ccc3fa
sontv <sontv@MacBook-Pro-cua-sontv.local>
2024-09-25 12:51:53 +0700

    Show Product

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,31 +3,57 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+const productList = [
+  {
+    id: 1,
+    name: "Product 1",
+    image: "https://picsum.photos/200/120",
+    description: "Desc 1",
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    image: "https://picsum.photos/200/120",
+    description: "Desc 2",
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    image: "https://picsum.photos/200/120",
+    description: "Desc 3",
+  },
+];
 
+function App() {
+  const [products, setProducts] = useState(productList);
   return (
     <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <table>
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Desc</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => {
+            return (
+              <tr>
+                <td>
+                  <img src={product.image} alt="" />
+                </td>
+                <td>{product.name}</td>
+                <td>{product.description}</td>
+                <td>
+                  <button>Xoa</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   );
 }
